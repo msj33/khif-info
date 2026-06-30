05cc4a40196d2faceaf8c83a187612f3cb2c6092
Morten Stensgaard <msj33@hotmail.com>
2026-06-30 23:34:37 +0200

    Updating secret

---
+++ admin/secret.js
@@ -0,0 +1,12 @@
+// Genereret lokalt med tools/encrypt-token.html
+window.KHIF_ADMIN_SECRET = {
+  owner: "msj33",
+  repo: "khif-info",
+  branch: "main",
+  contentPath: "content/pages.json",
+  adminUsername: "khif",
+  iterations: 250000,
+  salt: "5MkmlHSMCbPYyjwflywKzg==",
+  iv: "c5YQ+5d0LYuDgO3R",
+  encryptedToken: "81Lak04iumaYmVDicYwTE+0zw5rsR5vOF+Sfuruzu8tH8r9un+DFp5k+fW3OeFRaBg+637a0J/8ZVTyDU1tLERW0FNW0FjCiF7ZbRDmLUDBzRsi9u7EZM4mJwNGNWb6CSOQuEZ35yTC+osVd6w=="
+};
